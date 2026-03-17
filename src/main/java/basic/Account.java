8a871dfc0b74ef444fe7afd01f1b71a1ecffac96
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-17 18:45:00 +0100

    Account class

---
+++ src/main/java/basic/Account.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Account {
+
+	// Variables (Attributes)
+
+	private String owner;
+	private double balance;
+
+	// Methods (Actions)
+
+	public void setOwner(String name) {
+		owner = name;
+	}
+
+}
--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -1,18 +1,24 @@
- package basic;   
+package basic;
 
 import java.util.Date;
-
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
+		// hello + date
+
 		Date date = new Date();
-		
-	System.out.println("Hello world! Today is " + date);
-	
-	
-			
-	
+
+		System.out.println("Hello world! Today is " + date);
+
+		// creating an account Instance
+
+		Account myAccount = new Account();
+
+		myAccount.setOwner("Anuska");
+
+		System.out.println("The account belongs to:" + "Anuska");
 
 	}
 
