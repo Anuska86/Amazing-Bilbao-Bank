ddf568b4bf1b9ca64b48245caf1fa4e754e59447
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-24 18:38:52 +0100

    Read

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -6,21 +6,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
+		// SETUP
+
 		Bank myBank = new Bank("International Java Bank");
 
 		myBank.addAccounts(new SavingsAccount("Lauren", 1200.0, 2.5));
 		myBank.addAccounts(new SavingsAccount("Anuska", 5000.0, 3.0));
 		myBank.addAccounts(new SavingsAccount("Jon", 9800.0, 5));
 
-		Account lauren = myBank.findAccount("Lauren");
-
-		if (lauren != null) {
-			lauren.deposit(100);
-			lauren.withdraw(30);
-			lauren.withdraw(2000);
-
-			lauren.printHistory();
-		}
+		// DISPLAYING THE INFO
 
 		myBank.showStatus();
 
@@ -28,6 +22,19 @@ public class HelloWorld {
 
 		System.out.println("--- Our VIP Customers ---");
 		vips.forEach(name -> System.out.println("⭐ " + name));
+
+		// INTERACTION
+
+		double money = Read.readDouble("How much you want to deposit? ");
+
+		// UPDATE
+
+		Account acc = myBank.findAccount("Anuska");
+
+		if (acc != null) {
+			acc.deposit(money);
+			acc.printHistory();
+		}
 	}
 
 }
---
+++ src/main/java/basic/Read.java
@@ -0,0 +1,39 @@
+package basic;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Read {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static double readDouble(String message) {
+		while (true) {
+			try {
+				System.out.print(message);
+				double value = scanner.nextDouble();
+				scanner.nextLine();
+				return value;
+			} catch (InputMismatchException e) {
+				// TODO: handle exception
+				System.out.println("❌ Error: You have to write a number (e.g:10.5)");
+				scanner.nextLine(); // CRITICAL: This delete the bad input out of the memory
+			}
+		}
+	}
+
+	public static int readInt(String message) {
+		while (true) {
+			try {
+				System.out.print(message);
+				int value = scanner.nextInt();
+				scanner.nextLine();
+				return value;
+			} catch (InputMismatchException e) {
+				// TODO: handle exception
+				System.out.println("❌ Error: You have to write an integer number (e.g:5");
+				scanner.nextLine(); // CLEAR
+			}
+		}
+	}
+
+}
