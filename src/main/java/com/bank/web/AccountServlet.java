99810e3ea2d46d1be58f62ff9ad5e5df0ccc2d0f
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-04-07 18:46:12 +0200

    account

--- src/main/java/com/bank/web/AccountServlet.java
+++ src/main/java/com/bank/web/AccountServlet.java
@@ -1,38 +1,59 @@
 package com.bank.web;
 
+import java.io.IOException;
+
+import bank.models.Account;
+import bank.models.SavingsAccount;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class AccountServlet
  */
-@WebServlet("/AccountServlet")
+@WebServlet("/account")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public AccountServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public AccountServlet() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+
+		java.io.PrintWriter out = response.getWriter();
+
+		Account myAcc = new SavingsAccount(0, "Susazhina", 48000.0, 0, "K8454w$");
+
+		out.println("<html><body style='font-family: sans-serif; text-align: center;'>");
+		out.println("<h1 style='color: #2c3e50;'>🏦 Bank Account Dashboard</h1>");
+		out.println("<hr>");
+		out.println("<p><strong>Account Holder:</strong>" + myAcc.getOwner() + "</p>");
+		out.println("<p><strong>Current Balance:</strong> <span style='color: green;'>$" + myAcc.getBalance()
+				+ "</span> </p>");
+		out.println("<a href='index.html'>Back to Home</a>");
+		out.println("</body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
