4fc8c5bf1126fd0ce33f10754ea53fec589fd427
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-04-28 19:09:15 +0200

    Update AccountDAO.java

--- src/main/java/bank/db/AccountDAO.java
+++ src/main/java/bank/db/AccountDAO.java
@@ -3,31 +3,41 @@ package bank.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AccountDAO {
 	private String dbPassword = System.getenv("DB_PASSWORD");
 	private String url = "jdbc:mysql://localhost:3306/amazing_bilbao_bank";
 
-	public boolean insertAccount(String owner, String type, String iban, double balance) {
-		String sql = "INSERT INTO accounts (owner_name, account_type, iban, balance, password) "
-				+ "VALUES (?, ?, ?, ?, (SELECT password FROM (SELECT password FROM accounts WHERE owner_name = ? LIMIT 1) as temp))";
+	public int insertAccount(String owner, String type, String iban, double balance, String password) {
+
+		String sql = "INSERT INTO accounts (owner_name, account_type, iban, balance, password) VALUES (?, ?, ?, ?, ?)";
 
 		try (Connection conn = DriverManager.getConnection(url, "root", dbPassword);
-				PreparedStatement ps = conn.prepareStatement(sql)) {
+				PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
 			ps.setString(1, owner);
 			ps.setString(2, type);
 			ps.setString(3, iban);
 			ps.setDouble(4, balance);
-			ps.setString(5, owner);
+			ps.setString(5, password);
 
-			int rowsAffected = ps.executeUpdate();
-			return rowsAffected > 0;
+			int rows = ps.executeUpdate();
 
-		} catch (Exception e) {
+			if (rows > 0) {
+				try (ResultSet rs = ps.getGeneratedKeys()) {
+					if (rs.next())
+						return rs.getInt(1);
+				}
+			}
+
+		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
 		}
+
+		return -1;
 
 	}
 
