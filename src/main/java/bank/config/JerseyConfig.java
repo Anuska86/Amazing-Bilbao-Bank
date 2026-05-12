0e91d46d9dc59251a22037d39a53f4629764ac02
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-05-12 19:55:59 +0200

    JerseyConfig

---
+++ src/main/java/bank/config/JerseyConfig.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package bank.config;
+
+import jakarta.ws.rs.ApplicationPath;
+
+import jakarta.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
+
+
+/**
+ * 
+ */
+@ApplicationPath("/")
+public class JerseyConfig extends ResourceConfig{
+
+	public JerseyConfig() {
+		packages("com.bank.web"); 
+		
+		register(JspMvcFeature.class);
+		
+		property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/");
+	}
+	
+	
+}
